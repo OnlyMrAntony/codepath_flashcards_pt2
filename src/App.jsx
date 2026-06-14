993754834ad3bb676d5623b1d17fba507ed4cf4b
Anthony Heang <antrithea12@gmail.com>
2026-06-14 00:10:36 -0700

    Initial commit for new repo

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import { useState } from 'react';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Trivia Flashcards!</h1>
+        <h2>How well do you know your trivia?</h2>
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
